1a7783af0af04611722c5bd278edb195e4a94155
berat-erkul <beraterkul00@gmail.com>
2024-12-22 03:45:29 +0300

    I added dependencies in pom.xml

---
+++ src/test/java/beraterkul/springdatatablecreation/SpringDataTableCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package beraterkul.springdatatablecreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataTableCreationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
