03b841b76c4cd92ff1a5337e37d5f92b7589d437
berat-erkul <beraterkul00@gmail.com>
2025-01-28 21:25:26 +0300

    Created and connected H2 DB

---
+++ src/main/java/berat_erkul/spring_data_table_creation/SpringDataTableCreationApplication.java
@@ -0,0 +1,13 @@
+package berat_erkul.spring_data_table_creation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataTableCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataTableCreationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/berat_erkul/spring_data_table_creation/SpringDataTableCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package berat_erkul.spring_data_table_creation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataTableCreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
